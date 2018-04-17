410b6984664c79f626525d89b27e3305511a7b50
zdcthomas <zdcthomas@yahoo.com>
2018-04-17 09:10:25 -0600

    TESTS instantiation

--- test/employee_test.rb
+++ test/employee_test.rb
@@ -3,6 +3,13 @@ require './lib/employee'
 
 class EmployeeTest < Minitest::Test
   def test_instantiation
+    employee_id = '5'
+    name = 'Sally Jackson'
+    role = 'Engineer'
+    start_date = '2015-01-01'
+    end_date = '2018-01-01'
+    employee = Employee.new(employee_id, name, role, start_date, end_date)
+    assert_instance of Employee, employee
   end
 
   def test_attributes
