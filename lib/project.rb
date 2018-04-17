07663fbd4a4807e3b2eaa6a8a5c77323cd6a460d
zdcthomas <zdcthomas@yahoo.com>
2018-04-17 09:37:24 -0600

    implements project class

---
+++ lib/project.rb
@@ -0,0 +1,13 @@
+require_relative '../modules/date_handler'
+class Project
+  attr_reader :project_id,
+              :name,
+              :start_date,
+              :end_date
+  def initialize(project_id, name, start_date, end_date)
+    @project_id = project_id.to_i
+    @name = name
+    @start_date = DateHandler.string_to_date(start_date)
+    @end_date = DateHandler.string_to_date(end_date)
+  end
+end
