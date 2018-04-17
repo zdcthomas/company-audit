3a0b0e4c6482e3d972910735232bc69601b0f33f
zdcthomas <zdcthomas@yahoo.com>
2018-04-17 09:41:46 -0600

    TESTS attributes of company class begin as empty arrays

--- test/company_test.rb
+++ test/company_test.rb
@@ -7,4 +7,11 @@ class CompanyTest < Minitest::Test
     company = Company.new
     assert_instance_of Company, company
   end
+
+  def test_it_has_attributes
+    company = Company.new
+    assert_equal [], company.employees
+    assert_equal [], company.projects
+    assert_equal [], company.timesheets
+  end
 end
