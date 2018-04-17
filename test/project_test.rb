4656c240074d65726c50ab8747a5390dc27bab23
zdcthomas <zdcthomas@yahoo.com>
2018-04-17 09:19:42 -0600

    TESTS attributes of project class

--- test/project_test.rb
+++ test/project_test.rb
@@ -7,4 +7,16 @@ class ProjectTest < Minitest::Test
     project = Project.new('123', 'Widget Maker', '2015-01-01', '2018-01-01')
     assert_instance_of Project, project
   end
+
+  def test_it_has_attributes
+    project_id = '123'
+    name = 'Widget Maker'
+    start_date = '2015-01-01'
+    end_date = '2018-01-01'
+    project = Project.new(project_id, name, start_date, end_date)
+    assert_equal 123, project.project_id
+    assert_equal name, project.name
+    assert_instance_of Date, project.start_date
+    assert_instance_of Date, project.end_date
+  end
 end
