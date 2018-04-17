beb5ead2f517431e2ff20cd667fcf8a3a6a6b00e
zdcthomas <zdcthomas@yahoo.com>
2018-04-17 11:03:40 -0600

    implements load projects

--- lib/company.rb
+++ lib/company.rb
@@ -1,5 +1,6 @@
 require_relative '../modules/date_handler'
 require_relative './employee'
+require_relative './project'
 require_relative './fileio'
 require 'pry'
 class Company
@@ -29,6 +30,25 @@ class Company
       end
     end
     temp_employess.each{|employee|@employees << employee}
+    return report(true)
+  end
+
+  def load_projects(path)
+    csv_read = FileIo.load(path)
+    temp_projects = []
+    csv_read.each do |project|
+      id = project[0]
+      name = project[1]
+      start_date = project[2]
+      end_date = project[3]
+      if id && name && start_date && end_date
+        temp_projects << Project.new(id, name, start_date, end_date)
+      else
+        failed = true
+        return report(false,'bad data')
+      end
+    end
+    temp_projects.each{|project|@projects << project}
     return report(true)
   end
 
