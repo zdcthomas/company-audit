d48fab53bcc979adb74b938730afb0204ea1f60f
zdcthomas <zdcthomas@yahoo.com>
2018-04-17 11:29:43 -0600

    TESTS load company

--- test/audit_test.rb
+++ test/audit_test.rb
@@ -1,10 +1,21 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+require './lib/company'
 require './lib/audit'
 
 class AuditTest < Minitest::Test
   def test_it_exists
     audit = Audit.new
     assert_instance_of Audit, audit
+  end
+
+  def test_load_company
+    audit = Audit.new
+    company = Company.new
+    company.load_employees('./data/employees.csv')
+    company.load_projects('./data/projects.csv')
+    company.load_timesheets('./data/timesheets.csv')
+    actual = audit.load_company(company)
+    assert_instance_of Company, actual
   end
 end
