b93d216841192b16c382b149a4bc38726e24fa97
zdcthomas <zdcthomas@yahoo.com>
2018-04-17 10:25:42 -0600

    TESTS load method in company class

--- test/company_test.rb
+++ test/company_test.rb
@@ -17,7 +17,21 @@ class CompanyTest < Minitest::Test
 
   def test_load_employees_returns_hash
     company = Company.new
-    filename = './data/employees.csv'
-    assert_instance_of Hash, company.load_employees(filename)
+    good_employees = './data/employees.csv'
+    loaded = company.load_employees(good_employees)
+    assert_instance_of Hash, loaded
+    assert loaded[:success]
+    refute loaded[:error]
+    refute company.employees.empty?
+  end
+
+  def test_load_returns_error_if_data_is_bad
+    company = Company.new
+    bad_employees = './data/bad_employees.csv'
+    loaded = company.load_employees(bad_employees)
+    assert_instance_of Hash, loaded
+    refute loader[:success]
+    assert_eqaul 'bad data', loaded[:error]
+    assert company.employees.empty?
   end
 end
