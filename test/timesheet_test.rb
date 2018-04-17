10065ccc9b04bf2ba6d1253cd6df3ff7c72482b8
zdcthomas <zdcthomas@yahoo.com>
2018-04-17 09:22:52 -0600

    TESTS attributes of class timesheet

--- test/timesheet_test.rb
+++ test/timesheet_test.rb
@@ -11,4 +11,15 @@ class TimesheetTest < Minitest::Test
     timesheet = Timesheet.new(employee_id, project_id, date, minutes)
     assert_instance_of Timesheet, timesheet
   end
+
+  def test_it_has_attributes
+    employee_id = '5'
+    project_id = '7'
+    date = '2015-01-01'
+    minutes = '120'
+    timesheet = Timesheet.new(employee_id, project_id, date, minutes)
+    assert_equal 5, timesheet.employee_id
+    assert_equal 7, timesheet.project_id
+    assert_instance_of Date, timesheet.date
+    assert_equal 120, timesheet.minutes
 end
