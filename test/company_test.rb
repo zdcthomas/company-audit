a5449d19527904a7af68bdb55d61f43427145926
zdcthomas <zdcthomas@yahoo.com>
2018-04-17 10:59:33 -0600

    TESTS load projects

--- test/company_test.rb
+++ test/company_test.rb
@@ -15,7 +15,7 @@ class CompanyTest < Minitest::Test
     assert_equal [], company.timesheets
   end
 
-  def test_load_employees_returns_hash
+  def test_load_employees_returns_success_hash
     company = Company.new
     good_employees = './data/employees.csv'
     loaded = company.load_employees(good_employees)
@@ -25,7 +25,7 @@ class CompanyTest < Minitest::Test
     refute company.employees.empty?
   end
 
-  def test_load_returns_error_if_data_is_bad
+  def test_load_bad_employees_returns_error_if_data_is_bad
     company = Company.new
     bad_employees = './data/bad_employees.csv'
     loaded = company.load_employees(bad_employees)
@@ -33,5 +33,25 @@ class CompanyTest < Minitest::Test
     refute loaded[:success]
     assert_equal 'bad data', loaded[:error]
     assert company.employees.empty?
+  end
+
+  def test_load_projects_returns_success_hash
+    company = Company.new
+    good_projects = './data/projects.csv'
+    loaded = company.load_projects(good_projects)
+    assert_instance_of Hash, loaded
+    assert loaded[:success]
+    refute loaded[:error]
+    refute company.projects.empty?
+  end
+
+  def test_load_bad_projects_returns_error_if_data_is_bad
+    company = Company.new
+    bad_projects = './data/bad_projects.csv'
+    loaded = company.load_projects(bad_projects)
+    assert_instance_of Hash, loaded
+    refute loaded[:success]
+    assert_equal 'bad data', loaded[:error]
+    assert company.projects.empty?
   end
 end
