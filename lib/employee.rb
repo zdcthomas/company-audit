745e50c0fecf6a76664b31fbeb757daf1a5087fb
zdcthomas <zdcthomas@yahoo.com>
2018-04-17 09:31:02 -0600

    implements employee class, fixes typos in employee_test

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,2 +1,15 @@
+require_relative '../modules/date_handler'
 class Employee
+  attr_reader :id,
+              :name,
+              :role,
+              :start_date,
+              :end_date
+  def initialize(employee_id, name, role, start_date, end_date)
+    @id = employee_id.to_i
+    @name = name
+    @role = role
+    @start_date = DateHandler.string_to_date(start_date)
+    @end_date = DateHandler.string_to_date(end_date)
+  end
 end
--- test/employee_test.rb
+++ test/employee_test.rb
@@ -20,7 +20,7 @@ class EmployeeTest < Minitest::Test
     end_date = '2018-01-01'
     employee = Employee.new(employee_id, name, role, start_date, end_date)
     assert_equal 5, employee.id
-    assert_equal 'Engineer', employee.engineer
+    assert_equal 'Engineer', employee.role
     assert_equal 'Sally Jackson', employee.name
     assert_instance_of Date, employee.start_date
     assert_instance_of Date, employee.end_date
